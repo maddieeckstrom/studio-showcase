abf005e296cc17b5efe7c72fc2b6e00a659a8301
Maddie <maddieeckstrom@gmail.com>
2024-02-16 14:30:32 -1000

    added placeholder image for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './index.css'
+import ssLogo from "./assets/images/ssLogo.png"
 
 export default function App() {
   return (
     <section>
-      <h1>website coming soon</h1>
+      <div className="logo">
+        <img src={ssLogo} alt="Studio Showcase website coming soon"></img>
+      </div>
     </section>
   )
 }
